4e3d60765077ea64af518e2b02e7ee9e86338059
BhaveshKumarBMIETGSSIPU <bhaveshkumarbk2004@gmail.com>
2025-02-03 16:15:13 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+int i,n,fact=1;
+printf("enter a no. to find factorial:");
+scanf("%d",&n);
+for (i=1,i<=n;i++)
+{
+fact.fact*i,
+}
+print("factorial=%d"fact),
+}
